a57977890ae8a580a0cab1b637e456bab80fe88b
dzoric1 <Gerasimov2099@mail.ru>
2023-04-24 15:27:04 +0800

    realized page 404

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import { MainPage, ComicsPage } from '../pages';
+import { MainPage, ComicsPage, Page404 } from '../pages';
 
 const App = () => {
 
@@ -9,14 +9,11 @@ const App = () => {
             <div div className="app" >
                 <AppHeader />
                 <main>
-                    <Switch>
-                        <Route exact path="/">
-                            <MainPage />
-                        </Route>
-                        <Route exact path="/comics">
-                            <ComicsPage />
-                        </Route>
-                    </Switch>
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/comics" element={<ComicsPage />} />
+                        <Route path="*" element={<Page404 />} />
+                    </Routes>
                 </main>
             </div >
         </Router>
